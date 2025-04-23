427ea5400088667e66267b624cb4e2ac0193f1f6
hangytong <hangytong@gmail.com>
2025-04-23 17:18:48 +0800

    update

--- email/main.py
+++ email/main.py
@@ -4,39 +4,45 @@ from projects_list import projects
 from post_telegram import send_photo_telegram
 import os
 from dotenv import load_dotenv
+import time
+from datetime import datetime, timedelta
 
 send_mail = False
 send_tele = True
 
 
-project = projects[1]
+for project in projects:
 
 
-project_folder = f"invoices/{project['project']}"
-if not os.path.exists(project_folder):
-    os.makedirs(project_folder)
+    project_folder = f"invoices/{project['project']}"
+    if not os.path.exists(project_folder):
+        os.makedirs(project_folder)
 
 
-invoice_no, no_of_invoices = invoice.get_invoice_no(project)
-start_date, end_date = invoice.get_start_end_date(project,no_of_invoices)
-invoice_items = invoice.get_invoice_items(project,start_date,end_date)
+    client_name = project['client name']
+    client_addy = project['client addy']
+    client_alias = project['client alias']
+    no_of_teams = project['no of teams']
+    start_time = project['start time']
+    payment_type = project['payment_type']
+    payment_addy = project['payment_address']
 
-client_name = project['client name']
-client_addy = project['client addy']
-client_alias = project['client alias']
-no_of_teams = project['no of teams']
-start_time = project['start time']
-payment_type = project['payment_type']
-payment_addy = project['payment_address']
+    invoice_no, no_of_invoices = invoice.get_invoice_no(project)
+    start_date, end_date = invoice.get_start_end_date(project,no_of_invoices)
 
+    now = datetime.now()
+    if start_date == (datetime.now() - timedelta(days=5)).strftime("%d %b'%y") :
 
+        invoice_items = invoice.get_invoice_items(project,start_date,end_date)
 
-doc_path = invoice.create_invoice(project_folder,client_name,client_addy,client_alias,invoice_no,invoice_items,no_of_teams,start_date,
-                   end_date,start_time,payment_type,payment_addy)
+        doc_path = invoice.create_invoice(project_folder,client_name,client_addy,client_alias,invoice_no,invoice_items,no_of_teams,start_date,
+                        end_date,start_time,payment_type,payment_addy)
 
-if send_tele:
-    caption = f"Invoice {client_alias}-{invoice_no} Generated"
-    send_photo_telegram(doc_path, caption)
+        if send_tele:
+            caption = f"Invoice {client_alias}-{invoice_no} Generated"
+            send_photo_telegram(doc_path, caption)
 
-if send_mail:
-    mail.send_email(project,end_date)
+        if send_mail:
+            mail.send_email(project,end_date)
+
+    time.sleep(5)
